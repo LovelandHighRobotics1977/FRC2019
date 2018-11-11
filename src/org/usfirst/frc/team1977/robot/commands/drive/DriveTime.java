07a5538571b291a9719f50ea93a9e373f5e70419
Landon Berg <ljb1d322@thompsonschools.org>
2018-11-10 23:25:33 -0700

    I removed ramping speed from the DriveTime class. I also changed it to work with percentage speeds more efficiently. Hopefully this will fix our problems with the autonomous.

--- src/org/usfirst/frc/team1977/robot/commands/drive/DriveTime.java
+++ src/org/usfirst/frc/team1977/robot/commands/drive/DriveTime.java
@@ -2,64 +2,45 @@ package org.usfirst.frc.team1977.robot.commands.drive;
 
 import org.usfirst.frc.team1977.robot.commands.CommandBase;
 
-import edu.wpi.first.wpilibj.command.Command;
 
 public class DriveTime extends CommandBase {
 
 	/*private final String commandName = "Drive Forward";
     private final String subsytemName = "Drive";*/
 
-    private final double vPowerMin, vPowerMax, RAMP_RATE = 0.25, rampInterval;
+    private final double vPowerMin, vPowerMax;
     private final long startTime, halfTime, endTime;
-    private long lastRamp;
-    private double driveSpeed;
 
-    public DriveTime(double minSpeedVolts, double maxSpeedVolts, int timeMillis) {
+    private double driveSpeed;
+    
+    //Percentage goes from -1 to 1 - See CTRE documentation http://www.ctr-electronics.com/downloads/api/java/html/index.html
+    public DriveTime(double minSpeedPercent, double maxSpeedPercent, int timeMillis) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-
         //requires(drive);
         
-        vPowerMin = minSpeedVolts;
-        vPowerMax = maxSpeedVolts;
+        vPowerMin = minSpeedPercent;
+        vPowerMax = maxSpeedPercent;
         startTime = System.currentTimeMillis();
         halfTime = startTime + (timeMillis / 2);
         endTime = startTime + timeMillis;
-        rampInterval = timeMillis / ((vPowerMax - vPowerMin) / RAMP_RATE);
         System.out.println("drive time is go");
     }
     
  // Called just before this Command runs the first time
     protected void initialize() {
         driveSpeed = vPowerMin;
-        lastRamp = System.currentTimeMillis();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        drive.drive(0, (driveSpeed / 12), 0);
-        if (System.currentTimeMillis() <= halfTime) {
-            if (System.currentTimeMillis() - lastRamp >= rampInterval) {
-                driveSpeed += (RAMP_RATE);
-                lastRamp = System.currentTimeMillis();
-            }
-        } else {
-            if (System.currentTimeMillis() - lastRamp >= rampInterval) {
-                driveSpeed -= (RAMP_RATE);
-                lastRamp = System.currentTimeMillis();
-            }
-        }
-        if (driveSpeed > vPowerMax) {
-            driveSpeed = vPowerMax;
-        } else if (driveSpeed < vPowerMin) {
-            driveSpeed = vPowerMin;
-        }
+        drive.drive(0, driveSpeed, 0);
     }
 	
 	// Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-    	System.out.println("Cur " + System.currentTimeMillis());
-    	System.out.println("End " + endTime);
+    	System.out.println("Current: " + System.currentTimeMillis());
+    	System.out.println("End: " + endTime);
         return (System.currentTimeMillis() >= endTime);
     }
 
