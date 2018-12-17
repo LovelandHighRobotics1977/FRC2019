5e7667a3b9f8d6a970a28f33fe9d97769f0303bd
robotics <robotics@LO0145W32076837.thompsonschools.org>
2018-12-17 14:45:06 -0700

    Worked with test board

--- src/org/usfirst/frc/team1977/robot/subsystems/MotorControllerOne.java
+++ src/org/usfirst/frc/team1977/robot/subsystems/MotorControllerOne.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team1977.robot.subsystems;
 import edu.wpi.first.wpilibj.command.Subsystem;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 public class MotorControllerOne extends Subsystem {
@@ -8,9 +10,13 @@ public class MotorControllerOne extends Subsystem {
 	public static MotorControllerOne instance;
 	//private Relay rollerMotor;
 	private TalonSRX Motor;
+	ControlMode outputMode = ControlMode.PercentOutput;
+
 	
 	MotorControllerOne(){
 		//ShooterMotor = new VictorSP(0);
+		Motor = new TalonSRX(4);
+		Motor = new TalonSRX(5);
 	}
 	public static MotorControllerOne getInstance() {
 		if (instance == null) {
@@ -21,15 +27,17 @@ public class MotorControllerOne extends Subsystem {
 	
 	public void shoot() {
 		//ShooterMotor.set(-.30);
+		Motor.set(outputMode, .5);
 	}
 	
 	public void suck() {
 		//ShooterMotor.set(.30);
+		Motor.set(outputMode, -.5);
 	}
 	
 	public void stop() {
 		System.out.println(Motor.get());
-		Motor.stopMotor();
+		Motor.set(outputMode, 0);
 	}
 	@Override
 	protected void initDefaultCommand() {
