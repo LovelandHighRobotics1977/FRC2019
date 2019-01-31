463bf3952516cd36bdb0ed3507ec108a8447387d
robotics <robotics@LO0145W32076837.thompsonschools.org>
2019-01-31 15:23:00 -0700

    commented out camera code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -128,21 +128,21 @@ public class Robot extends TimedRobot {
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run();
 
-		//Camera Code
-		NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-		NetworkTableEntry tx = table.getEntry("tx");
-		NetworkTableEntry ty = table.getEntry("ty");
-		NetworkTableEntry ta = table.getEntry("ta");
+		// //Camera Code
+		// NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+		// NetworkTableEntry tx = table.getEntry("tx");
+		// NetworkTableEntry ty = table.getEntry("ty");
+		// NetworkTableEntry ta = table.getEntry("ta");
 
-		//read values periodically
-		double x = tx.getDouble(0.0);
-		double y = ty.getDouble(0.0);
-		double area = ta.getDouble(0.0);
+		// //read values periodically
+		// double x = tx.getDouble(0.0);
+		// double y = ty.getDouble(0.0);
+		// double area = ta.getDouble(0.0);
 
-		//post to smart dashboard periodically
-		SmartDashboard.putNumber("LimelightX", x);
-		SmartDashboard.putNumber("LimelightY", y);
-		SmartDashboard.putNumber("LimelightArea", area);
+		// //post to smart dashboard periodically
+		// SmartDashboard.putNumber("LimelightX", x);
+		// SmartDashboard.putNumber("LimelightY", y);
+		// SmartDashboard.putNumber("LimelightArea", area);
 	}
 
 	/**
