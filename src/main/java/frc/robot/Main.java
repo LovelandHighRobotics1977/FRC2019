bf81557d06fb6c9d2ec9f62d7eddb65f0a2f8b12
robotics <robotics@LO0145W28076833.thompsonschools.org>
2019-10-14 15:30:53 -0600

    Intro to programming

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -23,6 +23,8 @@ public final class Main {
    *
    * <p>If you change your main robot class, change the parameter type.
    */
+
+   //Welcome to Programming Team!
   public static void main(String... args) {
     RobotBase.startRobot(Robot::new);
   }
